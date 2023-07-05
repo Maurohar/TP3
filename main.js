7b6c6540735b70066b68fd6d5b4cd8259ef55f65
Mauro Harmitton <mauro.harmittonbr@gmail.com>
2023-07-05 17:11:34 -0500

    commit 5/7

--- main.js
+++ main.js
@@ -1,22 +1,40 @@
 //local Storage Variable
 let l = localStorage;
 
+const formularioContacto = document.getElementById("idFormulario");
 
- //Agarrando elementos del DOM
-let getData = function(){
-let nombreUsuario = document.getElementById("validacionNombre").value
-let apellidoUsuario = document.getElementById("validacionApellido").value
-let emailUsuario = document.getElementById("validacionMail").value
-let contraseniaUsuario = document.getElementById("validacionPassword").value
-let enviarUsuario = document.getElementById("botonEnviar").value
+
+formularioContacto.addEventListener("submit",(e)=>{
+    e.preventdefault();
+    const email = document.getElementById("email").value;
+    if(email.trim().lenght <= 3 || !email.includes("@")){
+    mostrarNotificacion("notificacionEmail", " el Mail debe tener más de 3 letras y contener '@'")
+    }
+});
+
+//Agarrando elementos del DOM
+let getData = function () {
+    let nombreUsuario = document.getElementById("validacionNombre").value
+    let apellidoUsuario = document.getElementById("validacionApellido").value
+    let emailUsuario = document.getElementById("validacionMail").value
+    let contraseniaUsuario = document.getElementById("validacionPassword").value
+    let enviarUsuario = document.getElementById("botonEnviar").value
 }
 
 
+function enviarDatosLocalStorage(obj1, obj2, obj3, obj4, obj5) {
+    // Almacenar las variables individualmente en el localStorage
+    localStorage.setItem('User', JSON.stringify(obj1));
+    localStorage.setItem('User', JSON.stringify(obj2));
+    localStorage.setItem('User', JSON.stringify(obj3));
+    localStorage.setItem('User', JSON.stringify(obj4));
+    localStorage.setItem('User', JSON.stringify(obj5));
+}
 
 
 class Usuario {
     constructor(id, nombre, apellido, email, contrasenia) {
-            this.id = id,
+        this.id = id,
             this.nombre = nombre,
             this.apellido = apellido,
             this.email = email,
@@ -38,22 +56,18 @@ const Objeto2 = new Usuario(2, "Pepe", "Benedetti", "pepebenedeti@gmail.com", 22
 
 const Objeto3 = new Usuario(3, "Juan", "Diablo", "juand@gmail.com", 33333)
 
-const Objeto4 = new Usuario(4, "Pagani", "Zonda", "pagani@gmail.com" , 44444)
+const Objeto4 = new Usuario(4, "Pagani", "Zonda", "pagani@gmail.com", 44444)
 
 const Objeto5 = new Usuario(5, "Alberto", "Ferraro", "alber@gmail.com", 55555)
 
 const Objeto6 = new Usuario(6, "Pedro", "Corola", "corola@gmail.com", 66666)
 
-const Usuario = []
-console.log(Usuario)
-Usuario.push(Objeto1, Objeto2, Objeto3, Objeto4, Objeto5, Objeto6)
-
 function agregarUsuario() {
-    let nombreIngresado = localStorage.setItem("Usuarios",JSON.stringify(Usuario))
-    let apellidoIngresado = localStorage.setItem("Usuarios",JSON.stringify(Usuario))
-    let emailIngresado = localStorage.setItem("Usuarios",JSON.stringify(Usuario))
-    let contraseniaIngresado = localStorage.setItem("Usuarios",JSON.stringify(Usuario))
-    const usuarioNuevo = new Usuario(id.length+1, nombreUsuario, apellidoUsuario, emailUsuario, contraseniaUsuario)
+    let nombreIngresado = localStorage.setItem("Usuarios", JSON.stringify(Usuario))
+    let apellidoIngresado = localStorage.setItem("Usuarios", JSON.stringify(Usuario))
+    let emailIngresado = localStorage.setItem("Usuarios", JSON.stringify(Usuario))
+    let contraseniaIngresado = localStorage.setItem("Usuarios", JSON.stringify(Usuario))
+    const usuarioNuevo = new Usuario(id.length + 1, nombreUsuario, apellidoUsuario, emailUsuario, contraseniaUsuario)
     usuarioNuevo.mostrarDatosObjetos()
     console.log(usuarioNuevo)
     Usuario.push(usuarioNuevo)
