054af5098e13e2f6189dabebe5f2fca83ca37a99
Mauro Harmitton <mauro.harmittonbr@gmail.com>
2023-07-04 22:24:05 -0500

    test hora 22:23

--- main.js
+++ main.js
@@ -3,22 +3,24 @@ let l = localStorage;
 
 
  //Agarrando elementos del DOM
+let getData = function(){
 let nombreUsuario = document.getElementById("validacionNombre").value
 let apellidoUsuario = document.getElementById("validacionApellido").value
 let emailUsuario = document.getElementById("validacionMail").value
 let contraseniaUsuario = document.getElementById("validacionPassword").value
 let enviarUsuario = document.getElementById("botonEnviar").value
+}
 
 
 
 
 class Usuario {
     constructor(id, nombre, apellido, email, contrasenia) {
-        this.id = id,
+            this.id = id,
             this.nombre = nombre,
             this.apellido = apellido,
-            this.email = email
-        this.contrasenia = contrasenia
+            this.email = email,
+            this.contrasenia = contrasenia
 
     }
     //los metodos en class se declaran fuera del constructor.
@@ -30,22 +32,21 @@ class Usuario {
 
 //interaciones con los autos.
 
-const Objeto1 = new Usuarios(1, "Mauro", "Harmitton", "mauro.harmittonbr@gmail.com", 11111)
+const Objeto1 = new Usuario(1, "Mauro", "Harmitton", "mauro.harmittonbr@gmail.com", 11111)
 
-const Objeto2 = new Usuarios(2, "Pepe", "Benedetti", "pepebenedeti@gmail.com", 22222)
+const Objeto2 = new Usuario(2, "Pepe", "Benedetti", "pepebenedeti@gmail.com", 22222)
 
-const Objeto3 = new Usuarios(3, "Juan", "Diablo", "juand@gmail.com", 33333)
+const Objeto3 = new Usuario(3, "Juan", "Diablo", "juand@gmail.com", 33333)
 
-const Objeto4 = new Usuarios(4, "Pagani", "Zonda", "pagani@gmail.com" , 44444)
+const Objeto4 = new Usuario(4, "Pagani", "Zonda", "pagani@gmail.com" , 44444)
 
-const Objeto5 = new Usuarios(5, "Alberto", "Ferraro", "alber@gmail.com", 55555)
+const Objeto5 = new Usuario(5, "Alberto", "Ferraro", "alber@gmail.com", 55555)
 
-const Objeto6 = new Usuarios(6, "Pedro", "Corola", "corola@gmail.com", 66666)
+const Objeto6 = new Usuario(6, "Pedro", "Corola", "corola@gmail.com", 66666)
 
 const Usuarios = []
 console.log(Usuarios)
 Usuarios.push(Objeto1, Objeto2, Objeto3, Objeto4, Objeto5, Objeto6)
-console.log(Usuarios)
 
 function agregarUsuarios() {
     let nombreIngresado = prompt("Ingrese la Marca del Automotor")
